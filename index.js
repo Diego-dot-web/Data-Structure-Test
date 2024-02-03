ebd5ab1d1784e1d1ac615755d21e84964d2a73b3
Diego <134121369+Diego-dot-web@users.noreply.github.com>
2024-02-02 22:43:56 -0500

    First Commit

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 function LinkedList(){
+    const append = (value)=>{
+        
+    }
 
+    const prepend = (value) => {
+
+    }
+
+    const size = () => {
+
+    }
+
+    const head = () => {
+
+    }
+
+    const tail = () => {
+
+    }
+
+    const at = (index) => {
+
+    }
+
+    const pop = () => {
+
+    }
+
+    const contains = (value) => {
+
+    } 
+
+    const find = (value) => {
+
+    }
+
+    const toString = () => {
+
+    }
 }
 
 function Node(value = null, nextNode = null){
