d57048bc368fabcf5e4f7cdf453fd7d349691b56
Diego <134121369+Diego-dot-web@users.noreply.github.com>
2024-02-07 08:59:32 -0500

    Finish Hashmap class and add HashSet class

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 class Node {
-    constructor(key = 0, value, nextNode = null) {
+    constructor(key = 0, value = null, nextNode = null) {
         this.key = key;
         this.value = value;
         this.nextNode = nextNode;
@@ -204,7 +204,7 @@ class HashMap {
         this.map = [];
         this.LoadFactor = 0.75;
         this.size = 16;
-        this.currentSize = 1;
+        this.currentSize = 0;
     }
 
     getMap(){
@@ -238,19 +238,19 @@ class HashMap {
     }
 
     has(key) {
-        let counter = 2;
+        let counter = 1;
         const hashKey = this.hash(key);
 
         this.map.forEach(node => {
             if(node.key === hashKey){
-                return true
+                return console.log("true")
+            }
+            if(counter === this.currentSize && node.key !== hashKey){
+                return console.log("false")
             }
             counter = counter + 1
         })
-
-        if(counter === this.currentSize){
-            return false
-        }
+        
     }
 
     remove(key) {
@@ -262,13 +262,130 @@ class HashMap {
             this.map.splice(hashKey, 1)
             return console.log("true")
         }
-
     }
 
-    filter() {
+    length() {
+        return console.log(this.currentSize)
+    }
+
+    clear() {
+        this.map = [];
+        this.LoadFactor = 0.75;
+        this.size = 16;
+        this.currentSize = 0;
+    }
+
+    keys() {
         this.map.forEach(node => {
             console.log(node.key)
         })
+    }
+
+    values() {
+        this.map.forEach(node => {
+            console.log(node.value)
+        })
+    }
+
+    entries() {
+        const formatedArrray = [];
+        this.map.forEach(node => {
+           formatedArrray[node.key] = [node.key, node.value];
+        })
+
+        console.log(formatedArrray)
+    }
+}
+
+class HashSet {
+    constructor() {
+        this.map = [];
+        this.LoadFactor = 0.75;
+        this.size = 16;
+        this.currentSize = 0;
+    }
+
+    getMap(){
+        return this.map
+    }
+
+    hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+
+        return hashCode % 16;
+    }
+
+    set(key){
+        this.map[this.hash(key)] = new Node(this.hash(key))
+        this.currentSize++
+    }
+
+    get(key) {
+        while(this.map[this.hash(key)] === undefined){
+            if(this.map[this.hash(key)] === undefined){
+                return null
+            }
+
+            return this.map[this.hash(key)].key
+        }
+    }
+
+    has(key) {
+        let counter = 1;
+        const hashKey = this.hash(key);
+
+        this.map.forEach(node => {
+            if(node.key === hashKey){
+                return console.log("true")
+            }
+            if(counter === this.currentSize && node.key !== hashKey){
+                return console.log("false")
+            }
+            counter = counter + 1
+        })
+        
+    }
+
+    remove(key) {
+        const hashKey = this.hash(key);
+
+        if(this.map[hashKey] === undefined){
+            return console.log("false")
+        } else {
+            this.map.splice(hashKey, 1)
+            return console.log("true")
+        }
+    }
+
+    length() {
+        return console.log(this.currentSize)
+    }
+
+    clear() {
+        this.map = [];
+        this.LoadFactor = 0.75;
+        this.size = 16;
+        this.currentSize = 0;
+    }
+
+    keys() {
+        this.map.forEach(node => {
+            console.log(node.key)
+        })
+    }
+
+    entries() {
+        const formatedArrray = [];
+        this.map.forEach(node => {
+           formatedArrray[node.key] = [node.key, node.value];
+        })
+
+        console.log(formatedArrray)
     }
 }
 
@@ -279,8 +396,5 @@ test.set("spoge", "bob2")
 test.set("squirtel", "number3")
 test.set("myself", "test4")
 test.set("carlos", "carlos1")
-test.set("carlos", "carlos2")
 test.set("carla", "carla1")
-test.remove("3")
-test.filter()
-console.log(test.getMap())
+test.entries()
