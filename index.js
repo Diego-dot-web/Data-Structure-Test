3cd8fd7229a5749aa6a3261eccbe56c96682fc50
Diego <134121369+Diego-dot-web@users.noreply.github.com>
2024-02-06 22:52:45 -0500

    Start HashMap

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 class Node {
-    constructor(value, nextNode = null) {
+    constructor(key = 0, value, nextNode = null) {
+        this.key = key;
         this.value = value;
         this.nextNode = nextNode;
     }
@@ -16,7 +17,7 @@ class LinkedList {
         let node = new Node(value);
         let actual;
 
-        if(!this.head){
+        if (!this.head) {
             this.head = node;
         } else {
             actual = this.head;
@@ -39,7 +40,7 @@ class LinkedList {
 
     // get Size
     getSize() {
-        return this.size ;
+        return this.size;
     }
 
     // Get the first node in the list 
@@ -51,12 +52,12 @@ class LinkedList {
     getTail() {
         let actual = this.head;
 
-        while(actual.nextNode !== null){
+        while (actual.nextNode !== null) {
             actual = actual.nextNode;
         }
 
-        if (actual.nextNode === null){
-            console.log(actual); 
+        if (actual.nextNode === null) {
+            console.log(actual);
         }
     }
 
@@ -65,8 +66,8 @@ class LinkedList {
         let actual = this.head;
         let counter = 0;
 
-        while (actual){
-            if (counter === index){
+        while (actual) {
+            if (counter === index) {
                 console.log(actual);
             }
 
@@ -81,11 +82,11 @@ class LinkedList {
     removeEnd() {
         let actual = this.head;
 
-        while(actual.nextNode.nextNode !== null){
+        while (actual.nextNode.nextNode !== null) {
             actual = actual.nextNode;
         }
 
-        if (actual.nextNode.nextNode === null){
+        if (actual.nextNode.nextNode === null) {
             actual.nextNode = null
         }
 
@@ -96,14 +97,14 @@ class LinkedList {
     contains(value) {
         let actual = this.head
 
-        while(actual) {
-            if (actual.value === value){
+        while (actual) {
+            if (actual.value === value) {
                 return true;
-            } 
-            
+            }
+
             actual = actual.nextNode;
         }
-        
+
         return false;
     }
 
@@ -112,63 +113,63 @@ class LinkedList {
         let actual = this.head
         let counter = 0;
 
-        while(actual) {
-            if (actual.value === value){
+        while (actual) {
+            if (actual.value === value) {
                 return counter;
-            } 
+            }
             counter++
             actual = actual.nextNode;
         }
-        
+
         return null;
     }
 
     // Insert At
-    insertAt(value, index){
-        if(index < 0 || index > this.size){
+    insertAt(value, index) {
+        if (index < 0 || index > this.size) {
             return;
         }
 
-        if(index === 0){
-            this.head = new Node(value , this.head);
+        if (index === 0) {
+            this.head = new Node(value, this.head);
             this.size++
         }
 
-        if(index > 0){
+        if (index > 0) {
             let actual = this.head;
             let counter = 0;
 
-            while(actual){
-               if(index - 1 === counter){
-                actual.nextNode = new Node(value, actual.nextNode)
-               } 
-               counter++
-               actual = actual.nextNode;
+            while (actual) {
+                if (index - 1 === counter) {
+                    actual.nextNode = new Node(value, actual.nextNode)
+                }
+                counter++
+                actual = actual.nextNode;
             }
 
             this.size++
         }
 
-        
+
     }
 
     // Remove at Index
     removeAt(index) {
-        if(index < 0 || index > this.size){
+        if (index < 0 || index > this.size) {
             return;
         }
 
-        if(index === 0){
+        if (index === 0) {
             this.head = this.head.nextNode;
             this.size--
         }
 
-        if(index > 0) {
+        if (index > 0) {
             let actual = this.head;
             let counter = 0;
 
-            while(actual) {
-                if (index - 1 === counter){
+            while (actual) {
+                if (index - 1 === counter) {
                     actual.nextNode = actual.nextNode.nextNode;
                 }
 
@@ -181,13 +182,13 @@ class LinkedList {
     }
 
     // print list 
-    printList(){
+    printList() {
         let actual = this.head;
         let finalString = '';
 
-        while(actual){
+        while (actual) {
             finalString += `( ${actual.value} ) -> `
-            if(actual.nextNode === null){
+            if (actual.nextNode === null) {
                 finalString += 'null'
             }
             actual = actual.nextNode;
@@ -197,12 +198,89 @@ class LinkedList {
     }
 }
 
+class HashMap {
 
-const linkedListTest = new LinkedList();
+    constructor() {
+        this.map = [];
+        this.LoadFactor = 0.75;
+        this.size = 16;
+        this.currentSize = 1;
+    }
 
-linkedListTest.prepend(50);
-linkedListTest.append(100);
-linkedListTest.append(200);
-linkedListTest.append(300);
+    getMap(){
+        return this.map
+    }
 
-linkedListTest.printList();
+    hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+
+        return hashCode % 16;
+    }
+
+    set(key, value){
+        this.map[this.hash(key)] = new Node(this.hash(key), value)
+        this.currentSize++
+    }
+
+    get(key) {
+        while(this.map[this.hash(key)] === undefined){
+            if(this.map[this.hash(key)] === undefined){
+                return null
+            }
+
+            return this.map[this.hash(key)].value
+        }
+    }
+
+    has(key) {
+        let counter = 2;
+        const hashKey = this.hash(key);
+
+        this.map.forEach(node => {
+            if(node.key === hashKey){
+                return true
+            }
+            counter = counter + 1
+        })
+
+        if(counter === this.currentSize){
+            return false
+        }
+    }
+
+    remove(key) {
+        const hashKey = this.hash(key);
+
+        if(this.map[hashKey] === undefined){
+            return console.log("false")
+        } else {
+            this.map.splice(hashKey, 1)
+            return console.log("true")
+        }
+
+    }
+
+    filter() {
+        this.map.forEach(node => {
+            console.log(node.key)
+        })
+    }
+}
+
+const test = new HashMap()
+
+test.set("patrick", "start1")
+test.set("spoge", "bob2")
+test.set("squirtel", "number3")
+test.set("myself", "test4")
+test.set("carlos", "carlos1")
+test.set("carlos", "carlos2")
+test.set("carla", "carla1")
+test.remove("3")
+test.filter()
+console.log(test.getMap())
